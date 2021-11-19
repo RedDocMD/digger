64672ec95eefa66167ffc47128eedaf9792afae3
Deep Majumder <deep.majumder2019@gmail.com>
2021-11-19 23:16:27 +0530

    Using go-github now

--- digger.go
+++ digger.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/v40/github"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	client := github.NewClient(nil)
+
+	// list all organizations for user "willnorris"
+	orgs, _, err := client.Organizations.List(context.Background(), "willnorris", nil)
+	if err != nil {
+		log.Println(err)
+	}
+	for _, org := range orgs {
+		fmt.Println(*org)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/RedDocMD/digger
 
 go 1.17
+
+require (
+	github.com/google/go-github/v40 v40.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+)
